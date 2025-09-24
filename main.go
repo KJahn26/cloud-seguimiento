f9f95ec0625a8be04f581e39f510d76db903606b
KJahn26 <jahn8916@gmail.com>
2025-09-24 10:06:53 -0500

    primeros pasos bootstrap

--- main.go
+++ main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"encoding/json"
+	"html/template"
+	"log"
+	"net/http"
+)
+
 func main() {
+	http.HandleFunc("/image", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		json.NewEncoder(w).Encode(r.Body)
+	})
+
+	log.Fatal(http.ListenAndServe(":9097", nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	tmpl.Execute(w, nil)
 }
