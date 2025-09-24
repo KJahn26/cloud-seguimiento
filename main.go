f3b36d7521d9437392684a3105f85dad2503b101
AppBlitz <fabianzapata98@outlook.com>
2025-09-24 07:53:05 -0500

    ready

--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
 func main() {
+	http.HandleFunc("/image", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		json.NewEncoder(w).Encode(r.Body)
+	})
+	log.Fatal(http.ListenAndServe(":9097", nil))
 }
