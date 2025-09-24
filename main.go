762af3d94cfa3f1cad93a6bb9133ef6edd7faa71
AppBlitz <fabianzapata98@outlook.com>
2025-09-24 12:27:17 -0500

    server ready

--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"os"
 )
 
 func main() {
 	var port string
-	var addr string
 
 	for {
 		fmt.Print("Ingrese el puerto que desea utilizar (ej:8000): ")
-		fmt.Scan(&port)
+		fmt.Scanf("%s", &port)
 
 		// Probar si el puerto está disponible
-		ln, err := net.Listen("tcp", ":"+addr)
+		ln, err := net.Listen("tcp", ":"+port)
 		if err != nil {
 			fmt.Printf("El puerto %s está en uso o no tienes permisos ❌\n", port)
 			continue
@@ -28,6 +29,12 @@ func main() {
 		break
 	}
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		hostname, _ := os.Hostname()
+		w.Header().Set("Content-type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"hostname": hostname, "port": port})
+	})
+
 	// Levantar servidor HTTP en el puerto elegido
-	log.Fatal(http.ListenAndServe(":"+addr, nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
