3974ed50b4b27f8b3ca58de93fe91ee9bf75f013
AppBlitz <fabianzapata98@outlook.com>
2025-09-24 11:53:37 -0500

    port

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/image", func(w http.ResponseWriter, r *http.Request) {
-		defer r.Body.Close()
-		json.NewEncoder(w).Encode(r.Body)
-	})
-	log.Fatal(http.ListenAndServe(":9097", nil))
+	var port string
+	var addr string
+
+	for {
+		fmt.Print("Ingrese el puerto que desea utilizar (ej:8000): ")
+		fmt.Scan(&port)
+
+		// Probar si el puerto está disponible
+		ln, err := net.Listen("tcp", ":"+addr)
+		if err != nil {
+			fmt.Printf("El puerto %s está en uso o no tienes permisos ❌\n", port)
+			continue
+		}
+
+		// Puerto libre → cerramos el listener temporal y salimos del loop
+		ln.Close()
+		fmt.Printf("El servidor esta escuchando el puerto %s ✅\n", port)
+		break
+	}
+
+	// Levantar servidor HTTP en el puerto elegido
+	log.Fatal(http.ListenAndServe(":"+addr, nil))
 }
